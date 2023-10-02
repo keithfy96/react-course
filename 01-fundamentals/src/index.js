80fc346aaab7961d9e9365ba8310d81d135c22f4
keithlim <keithlimek@gmail.com>
2023-10-02 23:50:39 +0800

    started first tutorial

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-
-import { books } from './books';
-import Book from './Book';
-
-function BookList() {
-  return (
-    <>
-      <h1>amazon best sellers</h1>
-      <section className='booklist'>
-        {books.map((book) => {
-          return <Book {...book} key={book.id} />;
-        })}
-      </section>
-    </>
-  );
+function Greeting() {
+  return <h2>My First Component</h2>;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<BookList />);
+root.render(<Greeting />);
