44f46b91125e17d89b9cc8605c6e5f34b41ac56d
Keithfy Lim <keithlimek96@gmail.com>
2023-10-04 18:35:02 +0800

    started with the amzn books tutorial

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,10 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL900_SR900,600_.jpg"
+    alt="Iron Flame (The Empyrean, 2)"
+  />
+);
+const Title = () => <h2>Iron Flame (The Empyrean, 2)</h2>;
+const Author = () => {
+  return <h4> Rebecca Yarros </h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
